feef74396106ecaf1fbaa0a0244690d316ebcf60
blakeshucart <78447214+blakeshucart@users.noreply.github.com>
2021-02-02 16:01:40 -0600

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -5,18 +5,29 @@ def fizzbuzz():
     num2=int(input("Last number in the list:"))
     while num1 <= num2:
         if num1% 3 == 0 and num1% 5 == 0:
-            print("fizzbuzz")
+            if num1<0:
+              print("-fizzbuzz")  
+            else:
+               print("fizzbuzz") 
             num1+=1
             continue
             
         elif num1% 3 == 0:
-            print("fizz")
+            if num1<0:
+              print("-fizz")  
+            else:
+               print("fizz") 
             num1+=1
             continue
+            
         elif num1% 5 == 0:
-            print("buzz")
+            if num1<0:
+              print("-buzz")  
+            else:
+               print("buzz") 
             num1+=1
             continue
+            
         else:
             print(num1)
             num1+=1
